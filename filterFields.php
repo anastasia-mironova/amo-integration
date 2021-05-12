226a82dee277b944063a52304ad44a0a0ea95ea3
anastasia mironova <anast.mir22@yandex.ru>
2021-05-12 14:14:44 +0300

    new comment

--- filterFields.php
+++ filterFields.php
@@ -1,12 +1,15 @@
 <?php
 /*
-
+@param object $data массив значений лида
+@return масиив полей и их значений
 */
 function filterFields($data)
 {
+    //получаем наименования полей, которые считываем
     $json = file_get_contents("fields.json");
     $jsonDecocdedField = json_decode($json, true);
     $requiredFieldValue = array();
+    //смотрим какие поля у нас есть в пользовательских полях, полученных от амо 
     if (isset($data)) {
         foreach ($data as $field) {
             foreach ($jsonDecocdedField as $jsonField) {
@@ -20,10 +23,11 @@ function filterFields($data)
     return $requiredFieldValue;
 };
 /*
-
+    @param object $data массив значений лида
+    @return масиив полей , которые надо прибавить 
 */
 function countValue($data)
-{
+{   
     $res = filterFields($data);
     $socialNetworks = json_decode(file_get_contents('fieldsValue.json'), true);
     $trafficMoodels = json_decode(file_get_contents('type.json'), true);
@@ -86,7 +90,8 @@ function countValue($data)
     return $newresult;
 }
     /*
-
+    @param string $id
+    функция определяющая, из какого столбца надо уменьшить значение при удалении
   */
 function columnOfDeletedFields($id)
 {   
--- webhook.php
+++ webhook.php
@@ -3,11 +3,15 @@ require_once("db.php");
 require_once("filterFields.php");
 require_once("auth.php");
 /*
-
+     @param string $id
+     @param string $type
+     @param string $column
+    функция записи новых сделок  в файл логов
 */
 function writeJSONLogFile($id, $type, $column)
-{
+{  
     $file = 'log.json';
+    // получения всего списка логов в массив
     $json = file_get_contents($file);
     $idArray = array();
     $jsonDecocded = json_decode($json, true);
@@ -16,6 +20,7 @@ function writeJSONLogFile($id, $type, $column)
             $idArray[] = $elem;
         }
     }
+    //добавление новой записи ф файл логов
     $idArray[] = array('id' => $id, 'type' => $type, 'date' => date("Y-m-d H:i:s"), 'column' => $column);
     $result = json_encode($idArray, JSON_FORCE_OBJECT);
     $fp = fopen($file, 'w');
@@ -24,29 +29,30 @@ function writeJSONLogFile($id, $type, $column)
 }
 
 
-/*
 
-*/
 if (isset($_POST['leads'])) {
+    /*
+        обработка вебхука добавления
+    */
     $lead = $_POST['leads'];
     if (isset($lead['add'])) {
 
         $conn = new mySQLConnection('leads');
-
+    //если существуют пользовательски поля, то ищем необходдимые, если нет передаем null
         if (isset($lead['add'][0]['custom_fields'])) {
             $field = countValue($lead['add'][0]['custom_fields']);
         } else {
             $field = countValue(null);
         }
         writeJSONLogFile($lead['add'][0]['id'], 'add', $field[0]);
-
+        //обновляем значение ячейки и общее количество конверсий
         $conn->updateRow($field[0], $conn->getOneValue($field[0], null, 'leads')[$field[0]] + 1,'leads');
         $conn->updateTotalAmount('leads');
         $conn->closeConn();
     }
     /*
-
-  */
+         обработка вебхука восстановления
+    */
     if (isset($lead['restore'])) {
         $conn = new mySQLConnection('leads');
         if (isset($lead['restore'][0]['custom_fields'])) {
@@ -60,8 +66,8 @@ if (isset($_POST['leads'])) {
         $conn->closeConn();
     }
     /*
-
-  */
+        обработка вебхука удаления 
+    */
     if (isset($lead['delete'])) {
         $conn = new mySQLConnection('leads');
         $field = columnOfDeletedFields($lead['delete'][0]['id']);
@@ -72,8 +78,8 @@ if (isset($_POST['leads'])) {
         writeJSONLogFile($lead['delete'][0]['id'], 'delete', $field['column']);
     }
     /*
-
-  */
+         обработка вебхука обновления, причем если заполнено поле дата оплаты, то учитываем событие, а если нет, не учитываем
+   */
     if (isset($lead['update'])) {
         writeJSONLogFile($lead['update'][0]['id'], 'update', null);
 
@@ -86,12 +92,14 @@ if (isset($_POST['leads'])) {
                    
                 
                     $conn = new mySQLConnection('income');
+                    //авторизуемся в амо
                     $apiConnect = auth();
+                    // тянем данные обновленной сделки
                     $leadUp = $apiConnect->leads()->getOne($lead['update'][0]['id']);
-
+                    // увеличиваем продажи 
                     $conn->updateRow($field[0], $conn->getOneValue($field[0], null,'sales')[$field[0]] + 1,'sales');
                     $conn->updateTotalAmount('sales');
-
+                    // увеличиваем доходы
                     $conn->updateRow($field[0], $conn->getOneValue($field[0], null, 'income')[$field[0]] + $leadUp->price, 'income');
                     $conn->updateTotalAmount('income');
                     $conn->closeConn();
