87bbe0069d64a3cdcd9936e5d91c530c9a2d066b
anastasia mironova <anast.mir22@yandex.ru>
2021-06-02 09:04:28 +0300

    feature: add google auth

--- index.js
+++ index.js
@@ -1,32 +1,77 @@
-import { GoogleAdsApi } from 'google-ads-api'
-import { enums } from "google-ads-api";
-const client = new GoogleAdsApi({
-    client_id: '466980095499-oab6des535r84c9vioegnnp6r21pmmbg.apps.googleusercontent.com',
-    client_secret: '7u844_RqnI0RHsG0U-DUjVBw',
-    developer_token: '0Co-7r8hMgfuJRC5rppecA',
+// import { GoogleAdsApi } from 'google-ads-api'
+// import { enums } from "google-ads-api";
+// const client = new GoogleAdsApi({
+//     client_id: '466980095499-oab6des535r84c9vioegnnp6r21pmmbg.apps.googleusercontent.com',
+//     client_secret: '7u844_RqnI0RHsG0U-DUjVBw',
+//     developer_token: '0Co-7r8hMgfuJRC5rppecA',
+// })
+
+// const customer = client.Customer({
+//     customer_id: "2405709396",
+//     // customer_account_id: '113-160-3923',
+//     // login_customer_id: '240-570-9396', // Optionally provide a login-customer-id
+//     // linked_customer_id: '789-789-789', // Optionally provide a linked-customer-id
+//     refresh_token: '1//04Hg808qTG3tBCgYIARAAGAQSNgF-L9Irifu6WKNObyGN82Mxd4O47HgNZZbde2VX7ogjLo-KcpfdUntBI4twLQrgoo4GUP5JSQ',
+// })
+
+
+
+// const campaigns = await customer.report({
+//     entity: "campaign",
+//     attributes: [
+//         "campaign.id",
+//         "campaign.name",
+//         "campaign.bidding_strategy_type",
+//         "campaign_budget.amount_micros",
+//     ],
+//     constraints: {
+//         "campaign.status": enums.CampaignStatus.ENABLED,
+//     },
+//     limit: 20
+// });
+// console.log(campaigns)
+
+import https from 'https'
+const token = 'AQAAAAAjg-SlAAcd6Fqy3PLVqkaVp1Jg5-CWj_U';
+
+const options = {
+    url: 'https://api.direct.yandex.com/json/v5/campaigns',
+    method: 'POST',
+    // headers: {
+    //     'Authorization': `Bearer ${token}`, // OAuth-токен. Использование слова Bearer обязательно
+    //     "Client-Login": "MCPRmarketing@yandex.ru", // Логин клиента рекламного агентства
+    //     "Accept-Language": "ru",
+    //     'Content-Type': "application/json; charset=utf-8"
+    // },
+    body: {
+        method: "get",
+        params: {
+            "SelectionCriteria": { /* CampaignsSelectionCriteria */
+
+                "States": ["ON"],
+            },
+            /* required */
+            "FieldNames": ["Id", "Name", "Type"]
+        }
+    }
+}
+
+const req = https.request(options, res => {
+    res.headers({
+        'Authorization': `Bearer ${token}`, // OAuth-токен. Использование слова Bearer обязательно
+        "Client-Login": "MCPRmarketing@yandex.ru", // Логин клиента рекламного агентства
+        "Accept-Language": "ru",
+        'Content-Type': "application/json, charset=utf-8"
+    })
+    console.log(`statusCode: ${res.statusCode}`)
+
+    res.on('data', d => {
+        process.stdout.write(d)
+    })
 })
 
-const customer = client.Customer({
-    customer_id: "2405709396",
-    // customer_account_id: '113-160-3923',
-    // login_customer_id: '240-570-9396', // Optionally provide a login-customer-id
-    // linked_customer_id: '789-789-789', // Optionally provide a linked-customer-id
-    refresh_token: '1//04Hg808qTG3tBCgYIARAAGAQSNgF-L9Irifu6WKNObyGN82Mxd4O47HgNZZbde2VX7ogjLo-KcpfdUntBI4twLQrgoo4GUP5JSQ',
+req.on('error', error => {
+    console.error(error)
 })
 
-
-
-const campaigns = await customer.report({
-    entity: "campaign",
-    attributes: [
-        "campaign.id",
-        "campaign.name",
-        "campaign.bidding_strategy_type",
-        "campaign_budget.amount_micros",
-    ],
-    constraints: {
-        "campaign.status": enums.CampaignStatus.ENABLED,
-    },
-    limit: 20
-});
-console.log(campaigns)
+req.end()
--- test.js
+++ test.js
@@ -1,11 +1,53 @@
 // содежимое index.js
-const http = require('http')
+
+import https from 'https'
 const port = 3000
 const requestHandler = (request, response) => {
-    console.log(request.url)
-    response.end('Hello Node.js Server!')
+    const token = 'AQAAAAAjg-SlAAcd6Fqy3PLVqkaVp1Jg5-CWj_U';
+
+    const options = {
+        url: 'https://api.direct.yandex.com/json/v5/campaigns',
+        method: 'POST',
+        // headers: {
+        //     'Authorization': `Bearer ${token}`, // OAuth-токен. Использование слова Bearer обязательно
+        //     "Client-Login": "MCPRmarketing@yandex.ru", // Логин клиента рекламного агентства
+        //     "Accept-Language": "ru",
+        //     'Content-Type': "application/json; charset=utf-8"
+        // },
+        body: {
+            method: "get",
+            params: {
+                "SelectionCriteria": { /* CampaignsSelectionCriteria */
+
+                    "States": ["ON"],
+                },
+                /* required */
+                "FieldNames": ["Id", "Name", "Type"]
+            }
+        }
+    }
+
+    const req = https.request(options, res => {
+        res.headers({
+            'Authorization': `Bearer ${token}`, // OAuth-токен. Использование слова Bearer обязательно
+            "Client-Login": "MCPRmarketing@yandex.ru", // Логин клиента рекламного агентства
+            "Accept-Language": "ru",
+            'Content-Type': "application/json, charset=utf-8"
+        })
+        console.log(`statusCode: ${res.statusCode}`)
+
+        res.on('data', d => {
+            process.stdout.write(d)
+        })
+    })
+
+    req.on('error', error => {
+        console.error(error)
+    })
+
+    req.end("jgjgj")
 }
-const server = http.createServer(requestHandler)
+const server = https.createServer(requestHandler)
 server.listen(port, (err) => {
     if (err) {
         return console.log('something bad happened', err)
